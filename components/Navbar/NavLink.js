3953ee0c588cef66103cf11585e8618b41bc822d
cormac.mccann1 <cormac.mccann@sylon.io>
2021-12-16 13:43:03 +0000

    styling for navigation component

--- components/Navbar/NavLink.js
+++ components/Navbar/NavLink.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from './styles/NavLink.module.css';
 
 const NavLink = (props) => {
   const router = useRouter();
+  console.log(styles.active);
   return (
     <Link href={props.path}>
-      <a className={router.pathname == "/" ? "active-link" : ""}>
+      <a className={ router.pathname === props.path ? styles.active : ""} >
         { props.text }
       </a>
     </Link>
---
+++ pages/posts/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+
+const Posts = () => (
+  <div  className={styles.home}>
+    <h1 data-testid="custom-element">Posts page</h1>
+  </div>
+)
+
+export default Posts;
