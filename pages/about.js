93f063816ae6706c6a220e6e4c37a7259d1cebae
cormac.mccann1 <cormac.mccann@sylon.io>
2022-01-04 11:56:06 +0000

    Small styling updates

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/About.module.css';
 
 const Home = () => (
-  <div  className="container main">
+  <div  className={`container main ${styles.aboutPage}`}>
     <h1 data-testid="custom-element">About</h1>
 
-    <p>Hi, I'm Cormac. Nice to meet you.</p>
+    <p className="text-s">Hi, I'm Cormac. Nice to meet you.</p>
 
     {/*  Image here? */}
 
@@ -13,7 +13,18 @@ const Home = () => (
 
     <p className="text-s">
       I'm a web developer from Dublin Ireland. I've been working been working primarily
-      as a frontend developer since January 2016.
+      as a frontend developer since January 2016. Since then I have worked in a variety of industries,
+    </p>
+
+    <h2 className="text-m">Outside of work</h2>
+
+    <p className="text-s">
+      During my down time I try to get away from the computer desk and spend as much
+      time as I can exploring my hobbies. Right now I'm learning photography so I've mainly been
+      going on nature walks to practice that.
+
+      If the weather isn't on my side, I'm usually at home either playing a video game or watching something on Netflix
+      (right now season 2 of The Witcher).
     </p>
   </div>
 )
